567ed718f5ae4903c48a61797f21e8fdc8e908c0
Jason Harrel <harrel.jason@gmail.com>
2019-06-01 21:44:42 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,3 +5,7 @@ end
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
+def introduction_with_language(name, language)
+
+takes in two arguments, a name and a language, and language defaults to Ruby (FAILED - 1)
