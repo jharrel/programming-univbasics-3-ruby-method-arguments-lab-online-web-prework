a16980ab09a8f29edbaf78dfdf7b14600250ab55
Jason Harrel <harrel.jason@gmail.com>
2019-06-01 21:45:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,8 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language(name, language)
-
-takes in two arguments, a name and a language, and language defaults to Ruby (FAILED - 1)
+describe "#introduction" do 
+  it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do 
+    expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
+  end
+end
