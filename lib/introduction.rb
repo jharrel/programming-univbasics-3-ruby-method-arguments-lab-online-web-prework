bbc301d2b5d2710bf1ae2e51a43731c9fd521c03
Jason Harrel <harrel.jason@gmail.com>
2019-06-01 21:45:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,8 +6,3 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-describe "#introduction" do 
-  it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do 
-    expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
-  end
-end
